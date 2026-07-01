3e3118f577b39f9f1222d69ad83de5f656d0ec86
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2026-07-01 12:31:06 +0100

    feat: shortcut switch kubeconfig and rename window
    Signed-off-by: Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>

--- config/zsh/tmux-sesh.zsh
+++ config/zsh/tmux-sesh.zsh
@@ -1,4 +1,4 @@
-
+# Create TMUX Sessions on Dir
 function _sesh_sessions_(){
     sesh list -t -c -H -d
 }
@@ -15,7 +15,48 @@ function _sesh_kill_(){
     tmux kill-session -t "$1"
 }
 
-function _session_runner_(){
+
+function sesh-connect-session() {
+    _select_and_handle_ "Select a TMUX Session" _sesh_sessions_ _sesh_connect_
+}
+zle     -N             sesh-connect-session
+
+function sesh-new-session() {
+    _select_and_handle_ "Start a TMUX Session" _sesh_all_ _sesh_connect_
+}
+zle     -N             sesh-new-session
+
+function sesh-kill-session() {
+    _select_and_handle_ "Kill a TMUX Session" _sesh_sessions_ _sesh_kill_
+}
+zle     -N             sesh-kill-session
+
+
+
+
+# Create TMUX Windows on Infra Session
+function _kubectl_contexts_(){
+    kubectl config get-contexts -o name
+}
+
+function _kubectl_switch_context_(){
+    kubectl config use-context "$1" > /dev/null
+    tmux rename-window "$1"
+}
+
+function kubectl-switch-context() {
+    _select_and_handle_ "Select Kubectl Context" _kubectl_contexts_ _kubectl_switch_context_
+}
+zle     -N             kubectl-switch-context
+bindkey '^[k' kubectl-switch-context # Alt-k
+
+
+
+
+
+
+# Run a selection prompt and run an action on an item
+function _select_and_handle_(){
     local title="$1"
     local target="$2"
     local action="$3"
@@ -30,20 +71,3 @@ function _session_runner_(){
 
     "$action" "$session"
 } 
-
-
-# Select TMUX Sesssion with Alt+A
-function sesh-connect-session() {
-    _session_runner_ "Select a TMUX Session" _sesh_sessions_ _sesh_connect_
-}
-zle     -N             sesh-connect-session
-
-function sesh-new-session() {
-    _session_runner_ "Start a TMUX Session" _sesh_all_ _sesh_connect_
-}
-zle     -N             sesh-new-session
-
-function sesh-kill-session() {
-    _session_runner_ "Kill a TMUX Session" _sesh_sessions_ _sesh_kill_
-}
-zle     -N             sesh-kill-session
