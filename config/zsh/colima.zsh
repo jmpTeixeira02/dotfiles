6e040109ad763c60ebff758ed94851cc2acd5c2b
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2026-09-03 12:47:06 +0100

    feat: colima start alias
    Signed-off-by: Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>

--- config/zsh/colima.zsh
+++ config/zsh/colima.zsh
@@ -1,2 +1,10 @@
 export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
 export DOCKER_HOST="unix://${HOME}/.config/colima/docker.sock"
+
+colima() {
+  if [[ "$1" == "start" ]]; then
+    command colima start --memory 8 --disk 200 --cpu 4 --dns 8.8.8.8
+  else
+    command colima "$@"
+  fi
+}
